727a7340a04c73a3c7b727b19f64ceb81b6bbcb9
oxape <wxdeve@126.com>
2017-02-05 11:20:37 +0800

    初始化工程

---
+++ RxSwiftWeather/Views/OXPRootViewController.swift
@@ -0,0 +1,20 @@
+//
+//  OXPRootViewController.swift
+//  RxSwiftWeather
+//
+//  Created by oxape on 2017/2/5.
+//  Copyright © 2017年 oxape. All rights reserved.
+//
+
+import UIKit
+
+class OXPRootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.brown
+    }
+
+}
